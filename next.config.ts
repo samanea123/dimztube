4003ebd885f1a96239a363e2f1305aa10ff4c687
Samanea Kuliner Junction Indonesia <samaneakulinerjunction.id@gmail.com>
2025-11-05 10:43:12 +0000

    Mana foto gue? Kenapa cuma U bro

--- next.config.ts
+++ next.config.ts
@@ -40,6 +40,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
